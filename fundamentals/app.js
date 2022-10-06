339f544c01b96d528680785605fec43c80ef8000
Saim Hashmi <saimhashmii433@gmail.com>
2022-10-06 17:06:14 +0500

    string methods and concatenation

--- fundamentals/app.js
+++ fundamentals/app.js
@@ -34,6 +34,15 @@
 
 
 
+
+
+
+
+
+
+
+
+
 /********* VARIABLES **********/
 
 // var, let, const
@@ -102,6 +111,13 @@
 
 
 
+
+
+
+
+
+
+
 /************** DATA TYPES ****************/
 
 // PRIMITIVE
@@ -130,6 +146,17 @@
 // const today = new Date();
 // console.log(today);
 // console.log(typeof today);
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -199,6 +226,13 @@
 
 
 
+
+
+
+
+
+
+
 /********* NUMBERS AND MATH OBJECTS **********/
 
 // const num1 = 100;
@@ -226,5 +260,89 @@
 // val = Math.random();
 
 // val = Math.floor(Math.random() * 20 + 1);
+
+// console.log(val);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/********* STRING METHODS AND CONCATENATION **********/
+
+// const firstName = 'William';
+// const lastName = 'Johnson';
+// const age = 36;
+// const str = 'Hello there my name is Brad';
+// const tags = 'web design,web development,programming';
+
+// let val;
+
+// val = firstName + lastName;
+
+// // Concatenation
+// val = firstName + ' ' + lastName;
+
+// // Append
+// val = 'Brad ';
+// val += 'Traversy';
+
+// val = 'Hello, my name is ' + firstName + ' and I am ' + age;
+
+// // Escaping
+// val =  'That\'s awesome, I can\'t wait';
+
+// // Length
+// val = firstName.length;
+
+// // concat()
+// val = firstName.concat(' ', lastName);
+
+// // Change case
+// val = firstName.toUpperCase();
+// val = firstName.toLowerCase();
+
+// val = firstName[2];
+
+// // indexOf()
+// val = firstName.indexOf('l');
+// val = firstName.lastIndexOf('l');
+
+// // charAt()
+// val = firstName.charAt('2');
+// // Get last char
+// val = firstName.charAt(firstName.length - 1);
+
+// // substring()
+// val = firstName.substring(0, 4);
+
+// // slice()
+// val = firstName.slice(0,4);
+// val = firstName.slice(-3);
+
+// // split()
+// val = str.split(' ');
+// val = tags.split(',');
+
+// // replace()
+// val = str.replace('Brad', 'Jack');
+
+// // includes()
+// val = str.includes('foo');
 
 // console.log(val);
