6573b5a2b4f07608d2eb7e6eb9b7a3cb3c8918db
Saim Hashmi <saimhashmii433@gmail.com>
2022-10-07 15:42:16 +0500

    dates and times

--- fundamentals/app.js
+++ fundamentals/app.js
@@ -561,3 +561,57 @@
 //   for(let i = 0; i < people.length; i++){
 //     console.log(people[i].name);
 //   }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/********* DATES AND TIMES **********/
+
+// let val;
+
+// const today = new Date();
+// let birthday = new Date('9-10-1981 11:25:00');
+// birthday = new Date('September 10 1981');
+// birthday = new Date('9/10/1981');
+
+// val = today.getMonth();
+// val = today.getDate();
+// val = today.getDay();
+// val = today.getFullYear();
+// val = today.getHours();
+// val = today.getMinutes();
+// val = today.getSeconds();
+// val = today.getMilliseconds();
+// val = today.getTime();
+
+// birthday.setMonth(0);
+// birthday.setDate(5);
+// birthday.setFullYear(1999);
+// birthday.setHours(3);
+// birthday.setMinutes(30);
+// birthday.setSeconds(25);
+
+// console.log(birthday);
