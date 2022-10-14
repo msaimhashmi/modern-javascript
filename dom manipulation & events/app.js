3c612745c785f0595b83ea591647d81d656f4ff0
Saim Hashmi <saimhashmii433@gmail.com>
2022-10-14 15:28:54 +0500

    DOM selectors for multiple elements

--- dom manipulation & events/app.js
+++ dom manipulation & events/app.js
@@ -56,6 +56,12 @@
 
 
 
+
+
+
+
+
+
 /********************* DOM SELECTORS FOR SINGLE ELEMENTS ***********************/
 
 
@@ -98,3 +104,90 @@
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/********************* DOM SELECTORS FOR MULTIPLE ELEMENTS ***********************/
+
+// // document.getElementsByClassName - it return HTMLCollection
+
+// // const items = document.getElementsByClassName('collection-item');
+// // console.log(items);
+// // console.log(items[0]);
+// // items[0].style.color = 'red';
+// // items[3].textContent = 'Hello';
+
+// // const listItems = document.querySelector('ul').getElementsByClassName('collection-item');
+
+// // // we can't run foreach loop directly because it requires array. we need to convert this collection into array to run foreach loop
+// // // listItems.forEach(li => {
+// // //   console.log(li);
+// // // });
+
+// // console.log(listItems);
+
+
+
+
+// // document.getElementsByTagName - it return HTMLCollection
+// // let lis = document.getElementsByTagName('li');
+// // console.log(lis);
+// // console.log(lis[0]);
+// // lis[0].style.color = 'red';
+// // lis[3].textContent = 'Hello';
+
+// // we can't run foreach loop directly because it requires array. we need to convert this collection into array to run foreach loop
+// // // Conver HTML Collection into array
+// // lis = Array.from(lis);
+
+// // lis.reverse();
+
+// // lis.forEach(function(li, index){
+// //   console.log(li.className);
+// //   li.textContent = `${index}: Hello`;
+// // });
+
+// // console.log(lis);
+
+
+
+// // document.querySelectorAll - it return NodeList
+
+// const items = document.querySelectorAll('ul.collection li.collection-item');
+
+// items.forEach(function(item, index){
+//     item.textContent = `${index}: Hello`;
+// });
+
+// const liOdd = document.querySelectorAll('li:nth-child(odd)');
+// const liEven = document.querySelectorAll('li:nth-child(even)');
+
+// // we can foreach loop directly because it return node lists.
+
+// liOdd.forEach(function(li, index){
+//   li.style.background = '#ccc';
+// });
+
+// for(let i = 0; i < liEven.length; i++){
+//   liEven[i].style.background = '#f4f4f4';
+// }
+
+// console.log(items);
