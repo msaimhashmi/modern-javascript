02eb989f5518c5807a41b0705681cd78aaff80c5
Saim Hashmi <saimhashmii433@gmail.com>
2022-10-17 15:30:24 +0500

    event listners and the event object

--- dom manipulation & events/app.js
+++ dom manipulation & events/app.js
@@ -420,3 +420,66 @@
 
 // console.log(val);
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/********************* EVENT LISTNERS AND THE EVENT OBJECTS ***********************/
+
+
+// // e.preventDefault(); - stop default behaviour of an element like <a> anchor tag have redirect behaviour on click.
+// // e.preventDefault(); - default behaviour ko stop karne k liye prevent default use hota hai! 
+
+// // document.querySelector('.clear-tasks').addEventListener('click', function(e){
+// //   console.log('Hello World');
+
+// //   //e.preventDefault();
+// // });
+
+// document.querySelector('.clear-tasks').addEventListener('click', onClick);
+
+// function onClick(e){
+//     e.preventDefault();
+//   //console.log('Clicked');
+
+//   let val;
+
+//   val = e;
+
+//   // Event target element
+//   val = e.target;
+//   val = e.target.id;
+//   val = e.target.className;
+//   val = e.target.classList;
+
+//   // Event type
+//   val = e.type;
+
+//   // Timestamp
+//   val = e.timeStamp;
+
+//   // Coords event relative to the window
+//   val = e.clientY;
+//   val = e.clientX;
+
+//   // Coords event relative to the element
+//   val = e.offsetY;
+//   val = e.offsetX;
+
+//   console.log(val);
+// }
