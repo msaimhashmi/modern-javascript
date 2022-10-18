5e06cacc311d9ed8d72cf5aadf7243aecd69af04
Saim Hashmi <saimhashmii433@gmail.com>
2022-10-18 17:13:53 +0500

    Tasklist [Part 2] - Delete and Filter Tasks

--- tasklist/app.js
+++ tasklist/app.js
@@ -1,5 +1,3 @@
-// Tasklist [PART 1] - UI & Add task items
-
 // Define UI Variables
 
 const form = document.querySelector('#task-form');
@@ -13,11 +11,20 @@ loadAllEventListeners();
 
 // Load all event listeners
 function loadAllEventListeners(){
-    
     // Add task event
     form.addEventListener('submit', addTask);
+
+    // Remove task event
+    taskList.addEventListener('click', removeTask);
+
+    // Clear tasks event
+    clearBtn.addEventListener('click', clearTasks);
+
+    // Filter task event
+    filter.addEventListener('keyup', filterTasks);
 }
 
+// Add task
 function addTask(e){
     if(taskInput.value === '')
     {
@@ -48,4 +55,41 @@ function addTask(e){
     taskInput.value = '';
 
     e.preventDefault();
+}
+
+// Remove task
+function removeTask(e){
+    if(e.target.parentElement.classList.contains('delete-item')){
+        if(confirm('Are you sure you want to delete?'))
+        {
+            e.target.parentElement.parentElement.remove();
+        }
+    }
+}
+
+// Clear task
+function clearTasks(){
+    // taskList.innerHTML = '';
+
+    // Fast
+    while(taskList.firstChild){
+        taskList.removeChild(taskList.firstChild);
+    }
+}
+
+// Filter task
+function filterTasks(e){
+    const text = e.target.value.toLowerCase();
+
+    document.querySelectorAll('.collection-item').forEach(function(task){
+        const item = task.firstChild.textContent;
+
+        if(item.toLowerCase().indexOf(text) != -1)
+        {
+            task.style.display = 'block';
+        }else{
+            task.style.display = 'none';
+        }
+        // console.log(item.toLowerCase().indexOf(text));
+    });
 }
