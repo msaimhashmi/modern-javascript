b1acf800a28da1c241fafbb0f95f3b7f76c6ade1
Saim Hashmi <saimhashmii433@gmail.com>
2022-10-20 13:44:19 +0500

    Tasklist [Part 3] - Persist To Local Storage (PROJECT COMPLETED)

--- tasklist/app.js
+++ tasklist/app.js
@@ -11,6 +11,9 @@ loadAllEventListeners();
 
 // Load all event listeners
 function loadAllEventListeners(){
+    // DOM load event
+    document.addEventListener('DOMContentLoaded', getTasks);
+
     // Add task event
     form.addEventListener('submit', addTask);
 
@@ -22,6 +25,41 @@ function loadAllEventListeners(){
 
     // Filter task event
     filter.addEventListener('keyup', filterTasks);
+}
+
+// Get tasks
+function getTasks()
+{
+    let tasks;
+    // lets keep in mind that localstorage save only in string 
+    // we need to convert data when we get from localstorage into JSON.parse when it comes out 
+    // then push new task to JSON value
+    if(localStorage.getItem('tasks') === null){
+        tasks = [];
+    }else{
+        tasks = JSON.parse(localStorage.getItem('tasks'));
+    }
+
+    tasks.forEach(function(task){
+        // Create li element
+        const li = document.createElement('li');
+        // Add class
+        li.className = 'collection-item';
+        // Create text node and append to li
+        li.appendChild(document.createTextNode(task));
+        
+        // Create a new link element
+        const link = document.createElement('a');
+        // Add class
+        link.className = 'delete-item secondary-content';
+        // Create inner HTML icon tag
+        link.innerHTML = '<i class="fa fa-remove"></i>';
+        // Append the link to li
+        li.appendChild(link);
+
+        // Append li to ul
+        taskList.appendChild(li);
+    });
 }
 
 // Add task
@@ -51,10 +89,31 @@ function addTask(e){
     // Append li to ul
     taskList.appendChild(li);
 
+    // Store task in local storage
+    storeTaskInLocalStorage(taskInput.value);
+
     // Clear the input
     taskInput.value = '';
 
     e.preventDefault();
+}
+
+// Store task
+function storeTaskInLocalStorage(task){
+    let tasks;
+    // lets keep in mind that localstorage save only in string 
+    // we need to convert data when we get from localstorage into JSON.parse when it comes out 
+    // then push new task to JSON value
+    if(localStorage.getItem('tasks') === null){
+        tasks = [];
+    }else{
+        tasks = JSON.parse(localStorage.getItem('tasks'));
+    }
+
+    tasks.push(task);
+
+    // convert JSON value to string to save data in localStorage
+    localStorage.setItem('tasks', JSON.stringify(tasks));
 }
 
 // Remove task
@@ -63,8 +122,35 @@ function removeTask(e){
         if(confirm('Are you sure you want to delete?'))
         {
             e.target.parentElement.parentElement.remove();
+            
+            // Remove task from local storage
+            removeTaskFromLocalStorage(e.target.parentElement.parentElement);
         }
     }
+}
+
+// Remove task from local storage
+function removeTaskFromLocalStorage(taskItem){
+    let tasks;
+    // lets keep in mind that localstorage save only in string 
+    // we need to convert data when we get from localstorage into JSON.parse when it comes out 
+    // then push new task to JSON value
+    if(localStorage.getItem('tasks') === null){
+        tasks = [];
+    }else{
+        tasks = JSON.parse(localStorage.getItem('tasks'));
+    }
+
+    tasks.forEach(function(task, index){
+        if(taskItem.textContent === task){
+            // first parameter index of item
+            // second parameter count of item
+            tasks.splice(index, 1);
+        }
+    });
+
+    // convert JSON value to string to save data in localStorage
+    localStorage.setItem('tasks', JSON.stringify(tasks));
 }
 
 // Clear task
@@ -75,6 +161,14 @@ function clearTasks(){
     while(taskList.firstChild){
         taskList.removeChild(taskList.firstChild);
     }
+
+    // Clear tasks from local storage
+    clearTasksFromLocalStorage();
+}
+
+// Clear tasks from local storage
+function clearTasksFromLocalStorage(){
+    localStorage.clear();
 }
 
 // Filter task
