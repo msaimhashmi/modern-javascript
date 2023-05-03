6c58831041396f52c182c3f61d5fc2dd43ddc420
Saim Hashmi <saimhashmii433@gmail.com>
2023-05-03 14:09:36 +0500

    HTTP requests

--- asynchronous javascript/app.js
+++ asynchronous javascript/app.js
@@ -1,3 +1,5 @@
+// EP 1
+
 // // SYNCHRONOUS
 // const syncWait = ms => {
 //     const end = Date.now() + ms
@@ -9,13 +11,49 @@
 // console.log(4);
 // console.log(5);
 
-// ASYNCHRONOUS
-console.log(1);
-console.log(2);
+// // ASYNCHRONOUS
+// console.log(1);
+// console.log(2);
 
-setTimeout(() => {
-    console.log('callback function fired!');
-}, 3000)
+// setTimeout(() => {
+//     console.log('callback function fired!');
+// }, 3000)
 
-console.log(3);
-console.log(4);
+// console.log(3);
+// console.log(4);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// EP 2
+// HTTP REQUESTS
+
+// We make http request to get/post/put (interact) data from another server/database etc.
+// We make these request to get data from another server to API endpoints
+
+const request = new XMLHttpRequest();
+
+// https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest/readyState
+request.addEventListener('readystatechange', ()=> {
+    // console.log(request, request.readyState);
+    if(request.readyState === 4){
+        console.log(request.responseText);
+    }
+});
+
+// https://jsonplaceholder.typicode.com/
+request.open('GET', 'https://jsonplaceholder.typicode.com/todos');
+request.send();
